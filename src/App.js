5d5936526a81f3424e53c3c88240bc58f2e2fe5c
Allen7702 <allen22052000@gmail.com>
2023-06-17 12:57:04 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
+import TableComponent from './TableComponent';
+import data from './data';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="App m-10">
+      <header className="m-6">
+        <h1 className="text-4xl text-gray-500 font-bold">HISP TANZANIA Physical Development Teaser</h1>
       </header>
+      <div className="App-content">
+        <TableComponent data={data} />
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
+
+
+<p>so based on that i want you to create a dynamic table that would look like this"
+i want the top row to have three columns period, Animal Region(fetched from the jsonobject i gave you below ) and Food Region(fetched from the jsonobject i gave you below ) then the second row
+ should divide the column of Animal Region and food region  to sub columns based on the number 
+ of data(values) they have  then from the third row downwards, to display the period values and
+   the values(data) of the data of  the 
+   animal region and food region for each column.
+</p>
+
+
+
+
---
+++ src/TableComponent.jsx
@@ -0,0 +1,123 @@
+import React from 'react';
+import './index.css';
+
+function TableComponent({ data }) {
+  const { headers, metaData, rows } = data;
+  const uniquePeriods = metaData.dimensions.pe;
+  const uniqueData = metaData.dimensions.dx;
+  const uniqueOUs = metaData.dimensions.ou.filter((ou) => ou !== "pe"); // Exclude "pe" from ou dimensions
+
+  const getColumnValues = (ou, period) => {
+    return uniqueData.map((data) => {
+      const matchingRow = rows.find((row) => row[0] === data && row[1] === ou && row[2] === period);
+      return matchingRow ? matchingRow[3] : "-";
+    });
+  };
+
+  return (
+    <table className="table-auto border  border-collapse">
+      <thead>
+        <tr>
+          <th className="border px-4 py-2"></th>
+          {uniqueOUs.map((ou, index) => (
+            <th key={index} className="border px-4 py-2" colSpan={uniqueData.length}>
+              {metaData.items[ou].name}
+            </th>
+          ))}
+        </tr>
+        <tr>
+          <th className="border px-4 py-2"></th>
+          {uniqueOUs.map((ou, index) => (
+            <React.Fragment key={index}>
+              {uniqueData.map((data, idx) => (
+                <th key={idx} className="border px-4 py-2">
+                  {metaData.items[data].name}
+                </th>
+              ))}
+            </React.Fragment>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {uniquePeriods.map((period) => (
+          <tr key={period}>
+            <td className="border px-4 py-2">{metaData.items[period].name}</td>
+            {uniqueOUs.map((ou, index) => (
+              <React.Fragment key={index}>
+                {getColumnValues(ou, period).map((value, idx) => (
+                  <td key={idx} className="border px-4 py-2">{value}</td>
+                ))}
+              </React.Fragment>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+export default TableComponent;
+
+
+
+
+
+
+
+
+
+
+// const TableComponent = () => {
+//   const { rows, metaData } = jsonData;
+//   const uniquePeriods = [...new Set(rows.map((item) => item[2]))];
+//   const uniqueData = [...new Set(rows.map((item) => item[0]))];
+//   const animalRegionRows = rows.filter((item) => item[1] === "zj9LoeErgkP");
+//   const foodRegionRows = rows.filter((item) => item[1] === "gwAcS6gaz7D");
+
+//   return (
+//     <table>
+//       <caption>Table</caption>
+//       <thead>
+//         <tr>
+//           <th> </th>
+//           <th colSpan={uniqueData.length}>Animal Region</th>
+//           <th colSpan={uniqueData.length}>Food Region</th>
+//         </tr>
+//         <tr>
+//           <th></th>
+//           {uniqueData.map((data, index) => (
+//             <th key={index}>{metaData.items[data].name}</th>
+//           ))}
+//           {uniqueData.map((data, index) => (
+//             <th key={index}>{metaData.items[data].name}</th>
+//           ))}
+//         </tr>
+//       </thead>
+//       <tbody>
+//         {uniquePeriods.map((period) => (
+//           <tr key={period}>
+//             <td>{metaData.items[period].name}</td>
+//             {uniqueData.map((data) => {
+//               const animalRegionRow = animalRegionRows.find(
+//                 (row) => row[0] === data && row[2] === period
+//               );
+//               return (
+//                 <td key={data}>{animalRegionRow ? animalRegionRow[3] : "-"}</td>
+//               );
+//             })}
+//             {uniqueData.map((data) => {
+//               const foodRegionRow = foodRegionRows.find(
+//                 (row) => row[0] === data && row[2] === period
+//               );
+//               return (
+//                 <td key={data}>{foodRegionRow ? foodRegionRow[3] : "-"}</td>
+//               );
+//             })}
+//           </tr>
+//         ))}
+//       </tbody>
+//     </table>
+//   );
+// };
+
+
